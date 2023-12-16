3bfcf308b6171ec1c6c519b6b569dc1eebcbad5e
Marcos de Brito <marcos.debrito@outlook.com>
2023-12-16 12:48:15 -0300

    first tests clojure

---
+++ src/blackjack/testsClojure.clj
@@ -0,0 +1,20 @@
+(ns blackjack.testsClojure)
+
+;simbolos -> "variaveis", porém aqui elas são imutáveis.
+;funcoes
+
+;Declarando as variáveis normalmente
+; var name = "Marcos"
+(def name "Marcos")
+(def age "21")
+(def numbers [1 3 6 8])
+(println numbers)
+
+;definindo uma funcao de saudacao -> defn
+(defn saudacao
+  "Saudacao iniciante em Clojure "
+  [name]
+  (str "Bem vindo " name))
+
+
+(println (saudacao name))
